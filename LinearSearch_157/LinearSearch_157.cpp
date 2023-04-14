47c96abb6da877608ca926033eaa5f9bb2d7573b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-14 10:45:02 +0700

    membuat display enter element array dan membuat variabel item

--- LinearSearch_157/LinearSearch_157.cpp
+++ LinearSearch_157/LinearSearch_157.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];										// Array to be searched
-int n;												// Number of element in the array
-int i;												// Index of array element
+int arr[20];																					// Array to be searched
+int n;																							// Number of element in the array
+int i;																							// Index of array element
 
 void input()
 {
@@ -31,7 +31,13 @@ void input()
 void LinearSearch()
 {
 	char ch;
-	int comparison;									// Number of Comparisons
+	int comparison;																					// Number of Comparisons
 
-
+	do
+	{
+		// Accept the number to eb searched
+		cout << "\n Enter element array  you wnt tp search ";										// Langkah 1
+		int item;
+		cin >> item
+	}
 }
