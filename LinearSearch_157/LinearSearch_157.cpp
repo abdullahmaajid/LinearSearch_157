398f7762db0f14db02343be2dd77b666c6b68ded
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-14 10:36:30 +0700

    membuat input array dengan menggunakan while

--- LinearSearch_157/LinearSearch_157.cpp
+++ LinearSearch_157/LinearSearch_157.cpp
@@ -6,4 +6,17 @@ int n;												// Number of element in the array
 int i;												// Index of array element
 
 void input()
-{}
+{
+	while (true)
+	{
+		cout << "Enter the number of element in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\n Array should have minimum 1 amd maximum 20 element. \n\n";
+	}
+
+	
+
+}
